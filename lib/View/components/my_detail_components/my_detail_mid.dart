33c1bdd56f933fe2aed46ae772693cf8e3a990c4
ARETEE <tajale01@gmail.com>
2022-10-19 10:24:31 +0545

    made top of my_detail page

---
+++ lib/View/components/my_detail_components/my_detail_mid.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyDetailMid extends StatelessWidget {
+  const MyDetailMid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 20.h,
+      ),
+      child: Row(
+        children: [
+          Container(),
+        ],
+      ),
+    );
+  }
+}
