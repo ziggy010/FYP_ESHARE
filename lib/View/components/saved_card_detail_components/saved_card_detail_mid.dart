d9e9391146035d3e0a2193593ebbddc620ef802d
ARETEE <tajale01@gmail.com>
2023-01-15 13:13:57 +0545

    saved card details mid

--- lib/View/components/saved_card_detail_components/saved_card_detail_mid.dart
+++ lib/View/components/saved_card_detail_components/saved_card_detail_mid.dart
@@ -7,32 +7,93 @@ class SavedCardDetailMid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 42.h,
-          width: 73.w,
-          decoration: BoxDecoration(
-            color: kContainerColor,
-            borderRadius: BorderRadius.circular(
-              10.r,
-            ),
-            border: Border.all(
-              color: kContainerColor,
-            ),
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 25.w,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ContactButton(
+                text: 'Call',
+                buttonColor: kContainerColor,
+                borderColor: kContainerColor,
+              ),
+              SizedBox(
+                width: 12.h,
+              ),
+              ContactButton(
+                text: 'Email',
+                buttonColor: Colors.transparent,
+                borderColor: kContainerColor,
+              ),
+            ],
           ),
-          child: Center(
-              child: Text(
-            'Call',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'poppins',
-              fontSize: 14.sp,
+          Container(
+            height: 42.h,
+            width: 92.w,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(
+                10.r,
+              ),
+              border: Border.all(
+                color: Color(0xFFFFDDDD),
+              ),
             ),
-          )),
+            child: Center(
+              child: Text(
+                'Delete',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontFamily: 'poppins',
+                  fontSize: 14.sp,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ContactButton extends StatelessWidget {
+  final String text;
+  final Color buttonColor;
+  final Color borderColor;
+
+  ContactButton({
+    required this.text,
+    required this.buttonColor,
+    required this.borderColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 42.h,
+      width: 73.w,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(
+          10.r,
         ),
-        Container(),
-      ],
+        border: Border.all(
+          color: borderColor,
+        ),
+      ),
+      child: Center(
+          child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'poppins',
+          fontSize: 14.sp,
+        ),
+      )),
     );
   }
 }
--- lib/View/saved_card_detail.dart
+++ lib/View/saved_card_detail.dart
@@ -1,4 +1,5 @@
 import 'package:e_share/Main%20files/constant.dart';
+import 'package:e_share/View/components/saved_card_detail_components/saved_card_detail_mid.dart';
 import 'package:e_share/View/components/saved_card_detail_components/saved_card_detail_top.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,6 +30,7 @@ class SavedCardDetail extends StatelessWidget {
         child: Column(
           children: const [
             SavedCardDetailTop(),
+            SavedCardDetailMid(),
           ],
         ),
       ),
